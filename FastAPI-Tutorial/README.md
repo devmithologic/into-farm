63823b2f260fb6fe0da5fd58d6fe1a8f8e35a89d
mithologic <devmithologic@gmail.com>
2025-11-20 15:08:23 -0600

    added delete and upload method

--- FastAPI-Tutorial/README.md
+++ FastAPI-Tutorial/README.md
@@ -32,3 +32,37 @@ poetry add 'fastapi-users[sqlalchemy]'
 poetry add 'uvicorn[standard]'
 poetry add aiosqlite
 ```
+
+---
+
+## Notes from FastAPI Tutorial
+
+Creating `app` folder is a common practice when developing a FastAPI application.
+
+
+### Getting into Imagekit.io
+
+This will be the API in which we can upload and manage all the image stuff
+[ImageKit.io](https://imagekit.io/dashboard/)
+
+From there is needed to create an account (it's free) and then go to developer options, from there we can get the values needed for our `.env` file such as our `PRIVATE_KEY`, `PUBLIC_KEY` and our `IMAGEKIT_URL`.
+
+
+### The main file
+
+We need to import uvicorn and with `uvicorn.run` we need to pass down the app argument constructed this way:
+
+`app.app:app` -> `folder.file:function`
+There is a folder `app/app.py` and within is a function called `app = FastAPI()`, so thats it.
+
+```python
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
+```
+---
+# FastAPI Concepts
+
+### Async
+TODO: Introduce documentation and concepts.
---
+++ FastAPI-Tutorial/app/app.py
@@ -0,0 +1,108 @@
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
+from typing import Optional
+from app.schemas import PostCreate, PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from contextlib import asynccontextmanager
+from app.images import imagekit
+from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+import shutil
+import os
+import uuid
+import tempfile
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+# This is like CREATE
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)
+):
+    temp_file_path = None
+    
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+            
+        upload_result = imagekit.upload_file(
+            file=open(temp_file_path, "rb"),
+            file_name=file.filename,
+            options=UploadFileRequestOptions(
+                use_unique_file_name=True,
+                tags=["backend-upload"]
+            )
+        )
+        
+        if upload_result.response_metadata.http_status_code == 200:
+            # Dependency injection
+            post = Post(
+                # All this are already part of the Post object at db.py
+                caption = caption,
+                url = upload_result.url,
+                file_type = "video" if (file.content_type and file.content_type.startswith("video/")) else "image",
+                file_name = upload_result.name
+            )
+            session.add(post)
+            await session.commit()
+            await session.refresh(post) # This is to create the missing data (id and createdat)
+            return post
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
+
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    # Execute a query selecting the 'Post' table and ordering by 'created_at' field
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    # List comprehension
+    posts = [row[0] for row in result.all()]
+    
+    posts_data = []
+    
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
+        
+    return {"posts": posts_data}
+
+@app.delete("/post/{post_id}")
+async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session)):
+    try:
+        post_uuid = uuid.UUID(post_id)
+        
+        result = await session.execute(select(Post).where(Post.id == post_uuid))
+        post = result.scalars().first()
+        
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+        
+        await session.delete(post)
+        await session.commit()
+        
+        return {"success": True, "message": "Post deleted", "deleted_post": str(post.id)}
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
